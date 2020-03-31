51119fd093a25d08006d22f12a51bc1685ded7fd
lkobetz <larissa.kobetz@gmail.com>
2020-03-31 14:28:35 -0400

    single and multiple votes return separately

--- index.js
+++ index.js
@@ -1,44 +1,94 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const rp = require("request-promise");
 
-request(
-  "https://www.goodreads.com/book/show/11866694-arcadia?ac=1&from_search=true&qid=VRQIpB3QKE&rank=2",
-  (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const wholePage = cheerio.load(html);
-      const allReviews = wholePage(".review");
-      let reviewsArr = Array.from(allReviews);
-      reviewsArr = reviewsArr.filter(review => {
-        // 5 is left bodycol // 1 is reviewHeader // 5 is rating
-        return (
-          review.children[5].children[1].children[5].attribs.title ===
-          "it was amazing"
-        );
-      });
-      const authorIds = [];
-      reviewsArr.forEach(review => {
-        let end = review.children[3].attribs.href.indexOf("-");
-        // the third child of the review contains review author info
-        authorIds.push(review.children[3].attribs.href.slice(11, end));
-      });
+const authorIds = [];
+
+const optionsReviews = {
+  uri:
+    "https://www.goodreads.com/book/show/15823480-anna-karenina?ac=1&from_search=true&qid=sUpwev6tfA&rank=1",
+  transform: function(html) {
+    return cheerio.load(html);
+  }
+};
+
+function makeRequest(options) {
+  return rp(options)
+    .then($ => {
+      return $;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+async function getReviewerIds() {
+  const $ = await makeRequest(optionsReviews);
+  const allReviews = await $(".review");
+  let reviewsArr = Array.from(allReviews);
+  reviewsArr = reviewsArr.filter(review => {
+    // 5 is left bodycol // 1 is reviewHeader // 5 is rating
+    if (review.children[5].children[1].children[5]) {
+      return (
+        review.children[5].children[1].children[5].attribs.title ===
+        "it was amazing"
+      );
+    }
+  });
+  reviewsArr.forEach(review => {
+    let end = review.children[3].attribs.href.indexOf("-");
+    // the third child of the review contains review author info
+    authorIds.push(review.children[3].attribs.href.slice(11, end));
+  });
+
+  return authorIds;
+}
+
+let all5Stars = { total: 0, books: {} };
+let multiples = {};
+let singles = [];
+
+async function getBookRecs(authorIds) {
+  for (let i = 0; i < authorIds.length; i++) {
+    let id = authorIds[i];
+    // let id = 1213607;
+    const $ = await makeRequest({
+      uri: `https://www.goodreads.com/review/list/${id}?shelf=read&sort=rating`,
+      transform: function(html) {
+        return cheerio.load(html);
+      }
+    });
+    const allTitles = await $(".field.title");
+    allTitlesArr = Array.from(allTitles);
+    allTitlesArr.forEach(title => {
+      all5Stars.total++;
+      if (title.children[1].children[1]) {
+        if (!(title.children[1].children[1].attribs.title in all5Stars.books)) {
+          all5Stars.books[title.children[1].children[1].attribs.title] = 1;
+        } else {
+          if (!(title.children[1].children[1].attribs.title in multiples)) {
+            multiples[title.children[1].children[1].attribs.title] = 1;
+          } else {
+            multiples[title.children[1].children[1].attribs.title]++;
+          }
+        }
+      } else {
+        all5Stars[id] = "no books";
+      }
+    });
+    for (let title in all5Stars.books) {
+      if (!(title in multiples)) {
+        singles.push(title);
+      }
     }
   }
-);
+  return { multiples, singles };
+}
 
-request(
-  "https://www.goodreads.com/review/list/1384944?shelf=read&sort=rating",
-  (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const usersReadShelf = cheerio.load(html);
-      const allTitles = usersReadShelf(".field.title");
-      allTitlesArr = Array.from(allTitles);
-      allTitlesArr.forEach(title => {
-        title.children[1].children[1] &&
-          console.log(
-            "NEW TITLE:",
-            title.children[1].children[1].attribs.title
-          );
-      });
-    }
-  }
+async function sortRecs(allBooks) {
+  console.log("allBooks:", allBooks);
+}
+
+getReviewerIds().then(result =>
+  getBookRecs(result).then(result => sortRecs(result))
 );
