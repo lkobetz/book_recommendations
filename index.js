9ed540e1d05cdb84ce6349150eedebc01f1cb3cb
lkobetz <larissa.kobetz@gmail.com>
2020-03-30 20:12:24 -0400

    only collects ids of reviewers who gave five star rating

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ request(
     if (!error && response.statusCode == 200) {
       const wholePage = cheerio.load(html);
       const allReviews = wholePage(".review");
-      const reviewsArr = Array.from(allReviews);
-      reviewsArr.forEach(review => {
-        console.log(review.children[3].attribs.title);
+      let reviewsArr = Array.from(allReviews);
+      reviewsArr = reviewsArr.filter(review => {
+        // 5 is left bodycol // 1 is reviewHeader // 5 is rating
+        return (
+          review.children[5].children[1].children[5].attribs.title ===
+          "it was amazing"
+        );
       });
+      const authorIds = [];
+      reviewsArr.forEach(review => {
+        let end = review.children[3].attribs.href.indexOf("-");
+        // the third child of the review contains review author info
+        authorIds.push(review.children[3].attribs.href.slice(11, end));
+      });
+      console.log(authorIds);
     }
   }
 );
